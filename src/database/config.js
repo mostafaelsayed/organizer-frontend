50b5dfe67a599723fe2cfb08d992bffa2511a79c
mostafaelsayed <mostafa.elsayed@ibm.com>
2020-01-01 22:03:12 +0200

    update apiUrl for different environments

--- src/database/config.js
+++ src/database/config.js
@@ -1,16 +1,18 @@
+const currentLocation = window.location.href;
 
+let apiUrl = '';
 
-// const config = {
-//     dev: {
-//         url: 'http://localhost:4000',
-//     },
-//     prod: {
-//         url: 'https://the-organizer-backend'
-//     }
-// };
-
-const apiUrl = window.location.href.startsWith('https') === true ? 'https://the-organizer-backend.herokuapp.com' : 'http://localhost:4000';
-
-
+// dev environment
+if (currentLocation.indexOf('the-organizer-dev') !== -1) {
+    apiUrl = 'https://the-organizer-backend-dev.herokuapp.com';
+}
+// staging environment
+else if (currentLocation.indexOf('the-organizer') !== -1) {
+    apiUrl = 'https://the-organizer-backend.herokuapp.com';
+}
+// local
+else {
+    apiUrl = 'http://localhost:4000';
+}
 
 export {apiUrl};
