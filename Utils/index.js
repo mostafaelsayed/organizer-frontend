5f6c121952a3732054608e409e109f56acfc8a14
mostafaelsayed <mostafaelsayed9419@gmail.com>
2025-04-06 00:32:45 +0200

    switch to vite

--- Utils/index.js
+++
@@ -1,7 +0,0 @@
-import checkPropTypes from 'check-prop-types';
-
-export const checkProps = (component, expectedProps) => {
-    const propsErr = checkPropTypes(component.propTypes, expectedProps, 'props', component.name);
-
-    return propsErr;
-}
--- src/data/reservations.js
+++
@@ -1,6 +0,0 @@
-const reservations = [
-    // {name: 'hotel reservation', id: 0},
-    // {name: 'doctor oppointment', id: 1}
-];
-
-export default reservations;
