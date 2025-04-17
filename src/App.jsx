a482aeb995d7c1941e6cb4ea9a34dd55b0e25c26
mostafaelsayed <mostafaelsayed9419@gmail.com>
2025-04-17 17:48:30 +0200

    basic react component to get users

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [emails, setEmails] = useState([]);
+  useEffect(() => {
+    async function getAllUsers () {
+      let response = await fetch('http://localhost:5000', {headers: {'Content-Type': 'application/json', 'Accept': 'application/json'}, method: 'POST', body: JSON.stringify({"query": "{getAllUsers{_data{id, email}, _statusCode}}"})});
+      const users = (await response.json()).data.getAllUsers._data.map(user => {
+        return {
+          email: user.email,
+          id: user.id
+        }
+      });
+      const emails = users.map(user => 
+        <li key={user.id}>{user.email}</li>
+      );
+      setEmails(emails);
+      
+    }
+    getAllUsers();
+  }, [])
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ul>
+      {emails}
+    </ul>
+  );
+  
 }
 
 export default App
